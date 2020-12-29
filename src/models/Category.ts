842b50e529c1189b8e32127d8f73351aef5a7ff5
Brener Alves <brener820@gmail.com>
2020-12-29 16:09:41 -0300

    desafio07

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -2,18 +2,23 @@ import {
   Entity,
   Column,
   PrimaryGeneratedColumn,
+  OneToMany,
   CreateDateColumn,
-  UpdateDateColumn
-} from "typeorm";
+  UpdateDateColumn,
+} from 'typeorm';
+
+import Transaction from './Transaction';
 
 @Entity('categories')
 class Category {
-
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
   title: string;
+
+  @OneToMany(() => Transaction, transaction => transaction.category)
+  transaction: Transaction;
 
   @CreateDateColumn()
   created_at: Date;
